854524bf2d6aa5f58a4bdadb522c9a73df0d8b8b
Sterling <Sterling@Mainframe>
2018-06-04 12:30:10 +0200

    Initial Java

---
+++ Datenbank 5.0/src/datenbank/pkg5/pkg0/main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datenbank.pkg5.pkg0;
+
+/**
+ *
+ * @author Sterling
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
