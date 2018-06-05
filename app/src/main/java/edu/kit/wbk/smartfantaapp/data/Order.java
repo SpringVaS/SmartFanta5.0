991afc4d618fde3276cfa7631b78e071c5882aba
Felix Heim <felix-heim@web.de>
2018-06-05 14:11:46 +0200

    corrected orders

--- app/src/main/java/edu/kit/wbk/smartfantaapp/data/Order.java
+++ app/src/main/java/edu/kit/wbk/smartfantaapp/data/Order.java
@@ -60,14 +60,14 @@ public class Order implements Serializable {
     }
 
     public static Order getOrderOne() {
-       String [][] o = {{"Magnet lang 313540200","1"},{"Getriebegehäuse Typ 4 1395107154","1"},{"Anker Typ 4 3134339434","6"},{"Bürstenhalter Typ 2 3134339434","1"}};
+       String [][] o = {{"i","1"},{"Getriebegehäuse Typ 4 1395107154","1"},{"Anker Typ 4 3134017958","6"},{"Buerstenhalter Typ 2 3134340867","1"}};
        String  v = "1";
        String a [] = {"2"};
        String [][] names = {{"Magnet lang","4"},{"Getriebegehäuse Typ 4","1"}, {"Anker Typ 4","4"}, {"Bürstenhalter Typ 2","3"}};
        return new Order(v,o,a,names,1,"start");
     }
     public static Order getOrderTwo() {
-       String [][] o = {{"Bürstenhalter Typ 2 3134340867","1"},{"Getriebedeckel (Typ 6)1391073305", "1"},{"Poltopf kurz 3135000183", "3"}};
+       String [][] o = {{"Buerstenhalter Typ 2 3134340867","1"},{"Getriebedeckel (Typ 6)1391073305", "1"},{"Poltopf kurz 3135000183", "3"}};
        String a []= {"1"};
        String v = "2";
        String [][] names = {{"Bürstenhalter Typ 2","3"}, {"Getriebedeckel (Typ 6)","1"},{"Poltopf kurz","4"}};
@@ -75,7 +75,7 @@ public class Order implements Serializable {
     }
     public static Order getRefillOrder() {
         String vnr = "1";
-        String otnr[][] = {{"Poltopf (lang) 3135000185","100"}};
+        String otnr[][] = {{"Poltopf 3135000183","100"}};
         String[] am = {"100"};
         String stnr = "Presse 4";
         String n [][]= {{"Poltopf lang","4"}};
